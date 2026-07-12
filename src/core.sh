f5b30b85e72dab7ce2472798d8a7508ae81d46cf
c92d58 <x@wahsun.org>
2026-07-12 13:54:35 +0800

    feat: 抬頭漸入動畫效果，首次顯示逐行載入

--- src/core.sh
+++ src/core.sh
@@ -1580,10 +1580,18 @@ update() {
 # main menu — geek style, vertical
 is_main_menu() {
     msg
-    msg " ${c_bright}+-----------------------------------------+${c_none}"
-    msg " ${c_bright}|${c_none}    ${c_bright}sbx ${is_sh_ver}${c_none}  ${is_core_status} ${c_bright}|${c_none}"
-    msg " ${c_bright}|${c_none}  ${c_dim}WAHSUN 2025-2026 MIT${c_none}   ${c_bright}|${c_none}"
-    msg " ${c_bright}+-----------------------------------------+${c_none}"
+    if [[ $is_sbx_splash == 0 ]]; then
+        msg " ${c_bright}+-----------------------------------------+${c_none}"; sleep 0.1
+        msg " ${c_bright}|${c_none}    ${c_bright}sbx ${is_sh_ver}${c_none}  ${is_core_status} ${c_bright}|${c_none}"; sleep 0.12
+        msg " ${c_bright}|${c_none}  ${c_dim}WAHSUN 2025-2026 MIT${c_none}   ${c_bright}|${c_none}"; sleep 0.12
+        msg " ${c_bright}+-----------------------------------------+${c_none}"; sleep 0.1
+        is_sbx_splash=1
+    else
+        msg " ${c_bright}+-----------------------------------------+${c_none}"
+        msg " ${c_bright}|${c_none}    ${c_bright}sbx ${is_sh_ver}${c_none}  ${is_core_status} ${c_bright}|${c_none}"
+        msg " ${c_bright}|${c_none}  ${c_dim}WAHSUN 2025-2026 MIT${c_none}   ${c_bright}|${c_none}"
+        msg " ${c_bright}+-----------------------------------------+${c_none}"
+    fi
     show_menu_items
     echo -ne " ${c_bright}>>${c_none} "
     read -r REPLY
@@ -1881,6 +1889,7 @@ main() {
         msg $tmp_port
         ;;
     main)
+        is_sbx_splash=0
         while :; do
             is_main_menu
         done
